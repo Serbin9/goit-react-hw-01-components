c3fee1410f6058e136ec40b02c691e0af89fa240
Serbin9 <serbin_y@ukr.net>
2020-03-06 14:19:28 +0200

    Updates

---
+++ precache-manifest.4498a785928057db78d90460539e8c02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8150b5c7aee1072d0787fcd29e25fab",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "698091dc615f2279c21f",
+    "url": "/goit-react-hw-01-components/static/css/main.22bf9c16.chunk.css"
+  },
+  {
+    "revision": "ef515206e8ee4e7d2cf4",
+    "url": "/goit-react-hw-01-components/static/js/2.757f7a6c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-01-components/static/js/2.757f7a6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "698091dc615f2279c21f",
+    "url": "/goit-react-hw-01-components/static/js/main.2754e37c.chunk.js"
+  },
+  {
+    "revision": "46dda3ea0057aa55cd36",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.dca0479d.js"
+  }
+]);
